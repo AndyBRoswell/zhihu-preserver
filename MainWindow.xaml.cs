f00f208ea335f0f03ee8ae54f84ce9c7421685f4
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-03-27 23:04:17 +0800

    /

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,12 +14,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace zhihu_preserver {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
 }
