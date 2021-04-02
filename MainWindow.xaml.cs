1a62cae15040a9527416cc1a6badf42f9f1fdfbf
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-04-03 02:32:37 +0800

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,8 @@ namespace zhihu_preserver {
 		internal List<IntPtr> Hwnd = new();
 		public MainWindow() {
 			InitializeComponent();
+			StreamReader reader = new StreamReader(Global.AppPath + "\\cfg\\config.cfg");
+			
 		}
 		private void Menu_Edit_New_Browser_Window_Click(object sender, RoutedEventArgs e) {
 			BrowserWindow browser = new();
