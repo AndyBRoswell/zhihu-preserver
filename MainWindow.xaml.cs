83a5344e2eda56502569e9fb1be700e593114ee9
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-04-02 22:10:31 +0800

    /

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,8 +22,7 @@ namespace zhihu_preserver {
 			InitializeComponent();
 		}
 		private void Menu_Edit_New_Browser_Window_Click(object sender, RoutedEventArgs e) {
-			BrowserWindow browser = new();
-			browser.Show();
+
 		}
 	}
 }
